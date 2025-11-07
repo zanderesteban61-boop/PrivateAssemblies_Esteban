6fe2733e72619756ce921a86ca1ac02afacc39b0
CAIRO <CAIRO@DESKTOP-G85462B>
2025-11-08 00:02:08 +0800

    final design

--- PrivateAssemblies_Esteban/FrmBasicCalculator.Designer.cs
+++ PrivateAssemblies_Esteban/FrmBasicCalculator.Designer.cs
@@ -121,6 +121,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(439, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnCompute);
